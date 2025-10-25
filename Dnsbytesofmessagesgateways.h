cd46786622a5718bac88994f9316317999b67135
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-25 14:41:40 +0200

    Create Dnsbytesofmessagesgateways.h

---
+++ Dnsbytesofmessagesgateways.h
@@ -0,0 +1,12 @@
+
+
+//there are 10^15 gateAus dor cou bytes rhat use logarithm.funcrion with push comma'ds.
+//the hypothesis here is that E have enough gateways.
+
+class safegatewayqonedummyforotherdumlies{
+
+public:
+
+
+//an algorithm that ned exp(3*10^15) pjlls commands
+ void linkcputogpusforvectors()
