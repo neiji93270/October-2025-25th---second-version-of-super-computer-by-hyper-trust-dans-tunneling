e1fecb9f648a5fc7ff0afec0bfd45477d7030df9
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-25 14:52:57 +0200

    Create Heatone.h

---
+++ Asyns/Heatone.h
@@ -0,0 +1,4 @@
+//there a re five variables in equations that
+//enable async commands on heat cpu behaviour for high energy cpu.
+//this is due to large scale ingeneering peoxesses that need both cpu and gpus
+//and because this behaviour also need log(10^17) parameters.
